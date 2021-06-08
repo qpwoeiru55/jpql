75088111b9d36c0548a373396ca3d7302af47a57
이진석 <wlstjrwlstjr18@gmail.com>
2021-06-08 17:45:59 +0900

    jaql

--- src/main/java/jpql/Main.java
+++ src/main/java/jpql/Main.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.Collection;
 import java.util.List;
 
 public class Main {
@@ -24,28 +25,58 @@ public class Main {
 //                    .getResultList();
 //            ==============================================================
 
-            Team team = new Team();
-            team.setName("teama");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("teamaA");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("jinseok");
-            member.setAge(10);
-            member.changeTeam(team);
-            member.setType(MemberType.ADMIN);
-            em.persist(member);
+            Team teamB  = new Team();
+            teamB.setName("teamaB");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setAge(10);
+            member1.changeTeam(teamA);
+            member1.setType(MemberType.ADMIN);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setAge(10);
+            member2.changeTeam(teamA);
+            member2.setType(MemberType.ADMIN);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setAge(10);
+            member3.changeTeam(teamB);
+            member3.setType(MemberType.ADMIN);
+            em.persist(member3);
+
+            em.flush();
+            em.clear();
 
 //            String query = "select m from Member m inner join m.team t";
 //            List<Member> result = em.createQuery(query, Member.class)
 //                    .setFirstResult(1)
 //                    .setMaxResults(10)
 //                    .getResultList();
-
-            String query = "select m.username, 'Hello', TRUE from Member m where m.type = :usertype";
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("usertype",MemberType.ADMIN)
+///////////////////////////////////////////////
+//            String query = "select m.username, 'Hello', TRUE from Member m where m.type = :usertype";
+//            List<Object[]> result = em.createQuery(query)
+//                    .setParameter("usertype",MemberType.ADMIN)
+//                    .getResultList();
+//            System.out.println(result);
+///////////////////////////////////////////////
+            String query = "select m from Member m join fetch m.team";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
-            System.out.println(result);
+
+            for(Member m : result){
+                System.out.println(m.getUsername() + m.getTeam().getName());
+            }
+
             tx.commit();
         }catch(Exception e){
             tx.rollback();
